d06fa009df643f483a856e9122dd1193e9c1a398
AdityaWahyuH <adit1tygmail.com>
2025-10-28 22:41:23 +0700

    bismillah

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import { GlobalContextProvider } from './context/context';
-import React from 'react'
+import React from 'react';
 import useNotifications from './Notifications/useNotifications';
-import { Snackbar } from '@ouroboros/react-native-snackbar';
 import AppNavigator from './AppNavigator';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+
+// Conditional import untuk Snackbar
+let Snackbar;
+if (Platform.OS !== 'web') {
+  // Hanya import di Android/iOS
+  const SnackbarModule = require('@ouroboros/react-native-snackbar');
+  Snackbar = SnackbarModule.Snackbar;
+}
 
 export default function App() {
   const navRef = useNotifications();
+  
   return (
     <GlobalContextProvider>
       <View style={{ flex: 1 }}>
         <StatusBar style='dark' />
         <AppNavigator navRef={navRef} />
 
-        <Snackbar actionStyle={{ color: "lightgreen" }} textStyle={{ color: "#bbb" }}
-          messageStyle={{ paddingVertical: 15, paddingHorizontal: 10, borderRadius: 3, backgroundColor: "black" }}
-        />
+        {/* Hanya render Snackbar di Android/iOS, skip di web */}
+        {Platform.OS !== 'web' && Snackbar && (
+          <Snackbar 
+            actionStyle={{ color: "lightgreen" }} 
+            textStyle={{ color: "#bbb" }}
+            messageStyle={{ 
+              paddingVertical: 15, 
+              paddingHorizontal: 10, 
+              borderRadius: 3, 
+              backgroundColor: "black" 
+            }}
+          />
+        )}
       </View>
-    </GlobalContextProvider >
+    </GlobalContextProvider>
   );
 }
